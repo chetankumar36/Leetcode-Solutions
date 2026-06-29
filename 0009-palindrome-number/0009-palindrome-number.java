c639ce8fd933b33a4f5021974a77bbe089417731
Chetan Kumar N K <chetannaikchetan866@gmail.com>
2026-06-29 22:40:19 +0530

    Time: 5 ms (79.54%), Space: 46 MB (53.13%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,19 +1,14 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String s = String.valueOf(x);
-        int left=0;
-        int right=s.length()-1;
-        for(int i=left;i<=right;i++){
-            if(s.charAt(i)==s.charAt(right)){
-                left++;
-                right--;
-            }
-            else{
-                return false;
-            }
+        int revalue=0;
+        int duplicate=x;
 
-        }
-        return true;
+        while(x>0){
+            int d1=x%10;
+            revalue=(revalue*10)+d1;
+            x/=10;
+            }
+            return revalue==duplicate;
         
 
 
